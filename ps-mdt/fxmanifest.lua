79edba0373d221671ab111fffa9229cc8ce49dc9
Gabbar Framework <gabbarframework@gmail.com>
2022-07-17 15:14:45 +0530

    Add files via upload

---
+++ ps-mdt/fxmanifest.lua
@@ -0,0 +1,37 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Flawws, Flakey, Idris and the Project Sloth team'
+description 'EchoRP MDT Rewrite for QBCore'
+version '0.9.9'
+
+lua54 'yes'
+
+shared_script 'shared/config.lua'
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/utils.lua',
+    'server/dbm.lua',
+    'server/main.lua'
+}
+client_scripts{
+    'client/main.lua',
+    'client/cl_impound.lua'
+} 
+
+ui_page 'ui/dashboard.html'
+
+files {
+    'ui/img/*.png',
+    'ui/img/*.webp',
+    'ui/dashboard.html',
+    'ui/dmv.html',
+    'ui/bolos.html',
+    'ui/incidents.html',
+    'ui/penalcode.html',
+    'ui/reports.html',
+    'ui/warrants.html',
+    'ui/app.js',
+    'ui/style.css',
+}
